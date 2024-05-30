24121dd41cb8da047ef4aa3ecfdf1266f36a6e39
TomasDo <58976265+tomas1108@users.noreply.github.com>
2024-05-30 10:53:36 +0700

    Update socket.js app Socket

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,10 @@ import io from "socket.io-client"; // Add this
 let socket;
 
 const connectSocket = (user_id) => {
-  socket = io("localhost:3001", {
+  // socket = io("localhost:3001", {
+  //   query: `user_id=${user_id}`,
+  // });
+    socket = io("http://bechat-env.eba-csvanjsa.ap-southeast-1.elasticbeanstalk.com", {
     query: `user_id=${user_id}`,
   });
 } // Add this -- our server will run on port 4000, so we connect to it from here
